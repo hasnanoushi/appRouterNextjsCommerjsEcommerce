c1cd24394f694a241552f41627970461e77df7da
HASNA KKP <hasnanoushi66@gmail.com>
2024-06-05 13:30:42 +0530

    commits

---
+++ src/app/lib/commerce.js
@@ -0,0 +1,9 @@
+import CommerceSDK from "@chec/commerce.js";
+
+const commerce = new CommerceSDK('pk_5706029c2e2bfc0eedb4b7868770e08a6ac575a64e2fc', {
+  headers: {
+    'X-Authorization': 'pk_5706029c2e2bfc0eedb4b7868770e08a6ac575a64e2fc',
+  },
+});
+
+export default commerce;
